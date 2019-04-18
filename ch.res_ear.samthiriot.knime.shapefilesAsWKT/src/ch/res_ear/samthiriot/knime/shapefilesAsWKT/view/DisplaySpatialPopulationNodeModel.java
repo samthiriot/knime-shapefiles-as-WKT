4ffd81a043bf5a4b207f4031e0f2325be1e478fa
Sam Thiriot <samuel.thiriot@res-ear.ch>
2019-04-18 15:20:23 +0200

    added the node for WKT visualisation

---
+++ ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesAsWKT/view/DisplaySpatialPopulationNodeModel.java
@@ -0,0 +1,296 @@
+package ch.res_ear.samthiriot.knime.shapefilesAsWKT.view;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.geotools.data.DataStore;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelColor;
+import org.knime.core.node.port.PortType;
+import org.knime.core.util.FileUtil;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import ch.res_ear.samthiriot.knime.shapefilesAsWKT.SpatialUtils;
+
+
+
+/**
+ * This is the model implementation of DisplaySpatialPopulation.
+ * View the spatial population on a map.
+ *
+ * @author Samuel Thiriot
+ */
+public class DisplaySpatialPopulationNodeModel extends NodeModel {
+    
+    // the logger instance
+    private static final NodeLogger logger = NodeLogger.getLogger(DisplaySpatialPopulationNodeModel.class);
+        
+    protected DataStore datastore1 = null;
+    protected File tmpFile1 = null;
+    
+    protected DataStore datastore2 = null;
+    protected File tmpFile2 = null;
+
+    protected SettingsModelColor m_color1 = new SettingsModelColor("color1", Color.GRAY);
+    protected SettingsModelColor m_color2 = new SettingsModelColor("color2", Color.BLUE);
+    
+    
+    /**
+     * Constructor for the node model.
+     */
+    protected DisplaySpatialPopulationNodeModel() {
+
+    	super(
+    			new PortType[] { BufferedDataTable.TYPE, BufferedDataTable.TYPE_OPTIONAL },
+    			new PortType[] { }
+    			);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+            final ExecutionContext exec) throws Exception {
+
+    	datastore1 = null;
+    	datastore2 = null;
+    			
+    	final BufferedDataTable inputPopulation1 = inData[0];
+    	
+    	final BufferedDataTable inputPopulation2 = inData[1];
+
+    	long total1 = inputPopulation1.size();
+    	long total2 = inputPopulation2 != null ? inputPopulation2.size() : 0;
+    	double totaltotal = total1+total2;
+    			
+    	// create progress monitors
+    	ExecutionMonitor progress1 = exec.createSubProgress(total1/totaltotal);
+    	ExecutionMonitor progress2 = exec.createSubProgress(total2/totaltotal);
+    	
+
+    	if (!SpatialUtils.hasGeometry(inputPopulation1.getDataTableSpec()))
+    		throw new IllegalArgumentException("the input table 1 contains no spatial data (no column named "+SpatialUtils.GEOMETRY_COLUMN_NAME+")");
+    	
+    	if (!SpatialUtils.hasCRS(inputPopulation1.getDataTableSpec()))
+    		throw new IllegalArgumentException("the input table 1 contains spatial data but no Coordinate Reference System");
+    	
+    	if (inputPopulation2 != null) {
+	    	if (!SpatialUtils.hasGeometry(inputPopulation2.getDataTableSpec()))
+	    		throw new IllegalArgumentException("the input table 2 contains no spatial data (no column named "+SpatialUtils.GEOMETRY_COLUMN_NAME+")");
+	    	
+	    	if (!SpatialUtils.hasCRS(inputPopulation2.getDataTableSpec()))
+	    		throw new IllegalArgumentException("the input table 2 contains spatial data but no Coordinate Reference System");
+	    	
+    	}
+    	
+    	// decode population 1
+    	CoordinateReferenceSystem crsOrig1 = SpatialUtils.decodeCRS(inputPopulation1.getSpec());
+    	tmpFile1 = FileUtil.createTempFile("shapefile", ".shp");
+
+    	// copy the input population into a datastore
+        datastore1 = SpatialUtils.createDataStore(tmpFile1, false);
+        Runnable runnableSpatialize1 = SpatialUtils.decodeAsFeaturesRunnable(
+        		inputPopulation1, 
+        		SpatialUtils.GEOMETRY_COLUMN_NAME, 
+        		progress1, 
+        		datastore1, 
+        		"entities1", 
+        		crsOrig1,
+        		false
+        		);
+    	exec.setMessage("storing entities");
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(runnableSpatialize1);
+        
+        
+        if (inputPopulation2 != null) {
+        	CoordinateReferenceSystem crsOrig2 = SpatialUtils.decodeCRS(inputPopulation2.getSpec());
+        	tmpFile2 = FileUtil.createTempFile("shapefile", ".shp");
+            datastore2 = SpatialUtils.createDataStore(tmpFile2, false);
+
+	        Runnable runnableSpatialize2 = SpatialUtils.decodeAsFeaturesRunnable(
+	        		inputPopulation2, 
+	        		SpatialUtils.GEOMETRY_COLUMN_NAME, 
+	        		progress2, 
+	        		datastore2, 
+	        		"entities2", 
+	        		crsOrig2,
+	        		false
+	        		);
+	        executor.execute(runnableSpatialize2);
+
+        }
+        
+        executor.shutdown();
+        try {
+        	// wait forever
+        	executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+        	throw new RuntimeException(e);
+        }
+    	
+        return new BufferedDataTable[]{};
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+      
+    	disposeDatastores();
+    }
+
+    protected void disposeDatastores() {
+    	 
+    	if (datastore1 != null)
+            datastore1.dispose();
+
+    	if (datastore2 != null)
+            datastore2.dispose();
+    }
+    
+    
+    
+    @Override
+	protected void onDispose() {
+    	disposeDatastores();
+    	super.onDispose();
+	}
+
+
+	/**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+            throws InvalidSettingsException {
+        
+        return new DataTableSpec[]{ };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+    	m_color1.saveSettingsTo(settings);
+    	m_color2.saveSettingsTo(settings);
+    	
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+
+    	m_color1.loadSettingsFrom(settings);
+    	m_color2.loadSettingsFrom(settings);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+            
+    	m_color1.validateSettings(settings);
+    	m_color2.validateSettings(settings);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+        
+    	{
+	    	File bckp = new File(internDir, "datastore1");
+	    	if (!bckp.exists() || !bckp.canRead() || !bckp.isFile())
+	    		return;
+	    	
+	    	System.out.println("restoring datastore from "+bckp);
+	
+	    	tmpFile1 = FileUtil.createTempFile("shapefile", ".shp");
+	    	
+	    	FileUtil.copy(
+	    			bckp,
+	    			tmpFile1,
+	    			exec
+	    			);
+	    	
+	        datastore1 = SpatialUtils.createDataStore(tmpFile1, false);
+    	}
+      	{
+	    	File bckp = new File(internDir, "datastore2");
+	    	if (!bckp.exists() || !bckp.canRead() || !bckp.isFile())
+	    		return;
+	    	
+	    	System.out.println("restoring datastore from "+bckp);
+	
+	    	tmpFile2 = FileUtil.createTempFile("shapefile", ".shp");
+	    	
+	    	FileUtil.copy(
+	    			bckp,
+	    			tmpFile2,
+	    			exec
+	    			);
+	    	
+	        datastore2 = SpatialUtils.createDataStore(tmpFile2, false);
+    	}
+    	
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+       
+    	if (datastore1 != null) {
+
+	    	File bckp = new File(internDir, "datastore1");
+	    	FileUtil.copy(
+	    			tmpFile1, 
+	    			bckp, 
+	    			exec.createSilentSubProgress(0.5)
+	    			);
+    	}
+    	if (datastore2 != null) {
+
+	    	File bckp = new File(internDir, "datastore2");
+	    	FileUtil.copy(
+	    			tmpFile2, 
+	    			bckp, 
+	    			exec.createSilentSubProgress(0.5)
+	    			);
+    	}
+    	
+    }
+
+}
+
---
+++ ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesAsWKT/view/DisplaySpatialPopulationNodeView.java
@@ -0,0 +1,155 @@
+package ch.res_ear.samthiriot.knime.shapefilesAsWKT.view;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
+
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.renderer.GTRenderer;
+import org.geotools.renderer.lite.StreamingRenderer;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapPane;
+import org.geotools.swing.tool.PanTool;
+import org.geotools.swing.tool.ScrollWheelTool;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeView</code> for the "DisplaySpatialPopulation" Node.
+ * View the spatial population on a map.
+ *
+ * @author Samuel Thiriot
+ */
+public class DisplaySpatialPopulationNodeView extends NodeView<DisplaySpatialPopulationNodeModel> {
+	
+    private static final NodeLogger logger = NodeLogger
+            .getLogger(DisplaySpatialPopulationNodeView.class);
+        
+	private MapContent content = null;
+	private JMapPane mapPane = null;
+
+
+    /**
+     * Creates a new view.
+     * 
+     * @param nodeModel The model (class: {@link DisplaySpatialPopulationNodeModel})
+     */
+    protected DisplaySpatialPopulationNodeView(final DisplaySpatialPopulationNodeModel nodeModel) {
+        super(nodeModel);
+
+        content = new MapContent();
+
+        GTRenderer renderer = new StreamingRenderer();
+	    mapPane = new JMapPane(content);
+	    mapPane.setRenderer(renderer);
+	    mapPane.addMouseListener(new ScrollWheelTool(mapPane));
+	    //mapPane.addMouseListener(new PanTool());
+	    
+	    /*
+	    try {
+		    PanTool panTool = new PanTool();
+		    panTool.setMapPane(mapPane);
+	    } catch (RuntimeException | NoClassDefFoundError e) {
+	    	e.printStackTrace();
+	    	logger.warn("unable to use panning: "+e.getMessage());
+	    }
+	    */
+	    
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	    
+        mapPane.setPreferredSize(new Dimension(screenSize.width*2/3, screenSize.height*2/3));
+       
+        setComponent(mapPane);
+        
+ 	   	
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void modelChanged() {
+
+        // TODO retrieve the new model from your nodemodel and 
+        // update the view.
+        DisplaySpatialPopulationNodeModel nodeModel = 
+            (DisplaySpatialPopulationNodeModel)getNodeModel();
+        assert nodeModel != null;
+        
+        // be aware of a possibly not executed nodeModel! The data you retrieve
+        // from your nodemodel could be null, emtpy, or invalid in any kind.
+        if (nodeModel.datastore1 == null)
+        	return;
+        
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onClose() {
+    
+        // TODO things to do when closing the view
+    	
+
+
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onOpen() {
+
+        DisplaySpatialPopulationNodeModel nodeModel = 
+                (DisplaySpatialPopulationNodeModel)getNodeModel();
+        
+        if (nodeModel == null || nodeModel.datastore1 == null) {
+        	System.err.println("No node model");
+        	return;
+        }
+        
+        try {
+	        // TODO things to do when opening the view
+	        
+        	// TODO remove ? content.removeLayer(layer)
+        	
+        	
+        	System.out.println("layers:" + nodeModel.datastore1.getNames());
+        	
+        	// add layer 1
+    		SimpleFeatureSource shapefileSource = nodeModel.datastore1.getFeatureSource(
+    				nodeModel.datastore1.getNames().get(0));
+    	    Style shpStyle = SLD.createPolygonStyle(nodeModel.m_color1.getColorValue(), nodeModel.m_color1.getColorValue(), 0.0f);
+    	    Layer shpLayer = new FeatureLayer(shapefileSource, shpStyle);       
+            content.addLayer(shpLayer);
+            mapPane.setDisplayArea(content.getMaxBounds());
+
+        	System.out.println("added layer 1");
+        	
+            // add layer 2
+            if (nodeModel.datastore2 != null) {
+            	System.out.println("display data source 2 "+nodeModel.datastore2.getNames());
+            	SimpleFeatureSource shapefileSource2 = nodeModel.datastore2.getFeatureSource(
+        				nodeModel.datastore2.getNames().get(0));
+        	    Style shpStyle2 = SLD.createPolygonStyle(nodeModel.m_color2.getColorValue(), null, 0.0f);
+        	    Layer shpLayer2 = new FeatureLayer(shapefileSource2, shpStyle2);       
+                content.addLayer(shpLayer2);
+
+            }
+          
+
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    
+    }
+
+}
+
