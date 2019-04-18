8ff19cd83b9706bc6c22803a070694321f017ac5
Sam Thiriot <samuel.thiriot@res-ear.ch>
2019-04-18 15:49:47 +0200

    added a node to reproject geometries

---
+++ ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesAsWKT/reproject/ReprojectNodeDialog.java
@@ -0,0 +1,37 @@
+package ch.res_ear.samthiriot.knime.shapefilesAsWKT.reproject;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+import ch.res_ear.samthiriot.knime.shapefilesAsWKT.DialogComponentReferenceSystem;
+import ch.res_ear.samthiriot.knime.shapefilesAsWKT.SpatialUtils;
+
+
+/**
+ * <code>NodeDialog</code> for the "Reproject" Node.
+ * Reprojects a spatialized population
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more 
+ * complex dialog please derive directly from 
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Samuel Thiriot
+ */
+public class ReprojectNodeDialog extends DefaultNodeSettingsPane {
+
+    /**
+     * New pane for configuring Reproject node dialog.
+     * This is just a suggestion to demonstrate possible default dialog
+     * components.
+     */
+    protected ReprojectNodeDialog() {
+        super();
+        
+        addDialogComponent(new DialogComponentReferenceSystem(
+        		new SettingsModelString(ReprojectNodeModel.MODEL_KEY_CRS, SpatialUtils.getDefaultCRSString()),
+        		"Coordinate Reference System"
+        		));
+    }
+}
+
