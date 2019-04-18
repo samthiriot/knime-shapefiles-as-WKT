43660458e97ec557bb5d844f3dcef732f03d6110
Sam Thiriot <samuel.thiriot@res-ear.ch>
2019-04-18 15:20:23 +0200

    added the node for WKT visualisation

---
+++ ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesAsWKT/view/DisplaySpatialPopulationNodeDialog.java
@@ -0,0 +1,43 @@
+package ch.res_ear.samthiriot.knime.shapefilesAsWKT.view;
+
+import java.awt.Color;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentColorChooser;
+import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
+import org.knime.core.node.defaultnodesettings.SettingsModelColor;
+import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
+
+/**
+ * <code>NodeDialog</code> for the "DisplaySpatialPopulation" Node.
+ * View the spatial population on a map.
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more 
+ * complex dialog please derive directly from 
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Samuel Thiriot
+ */
+public class DisplaySpatialPopulationNodeDialog extends DefaultNodeSettingsPane {
+
+    /**
+     * New pane for configuring DisplaySpatialPopulation node dialog.
+     * This is just a suggestion to demonstrate possible default dialog
+     * components.
+     */
+    protected DisplaySpatialPopulationNodeDialog() {
+        super();
+        
+        addDialogComponent(new DialogComponentColorChooser(
+        		new SettingsModelColor("color1", Color.GRAY),
+        		"color for entities 1 (top table)",
+        		true));
+        addDialogComponent(new DialogComponentColorChooser(
+        		new SettingsModelColor("color2", Color.BLUE),
+        		"color for entities 1 (bottom table)",
+        		true));
+
+    }
+}
+
