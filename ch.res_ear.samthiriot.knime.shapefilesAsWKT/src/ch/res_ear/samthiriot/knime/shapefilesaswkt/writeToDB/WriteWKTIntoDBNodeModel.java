106219d4e82d98f0e585286e89dcd68328f87192
sthiriot <sthiriot@EIFLC284.eifer.kit.edu>
2019-06-03 11:47:05 +0200

    added copyright notice (fix #32)

--- ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesaswkt/writeToDB/WriteWKTIntoDBNodeModel.java
+++ ch.res_ear.samthiriot.knime.shapefilesAsWKT/src/ch/res_ear/samthiriot/knime/shapefilesaswkt/writeToDB/WriteWKTIntoDBNodeModel.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2019 EIfER[1] (European Institute for Energy Research).
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU GENERAL PUBLIC LICENSE
+ * which accompanies this distribution, and is available at
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Contributors:
+ *     Samuel Thiriot - original version and contributions
+ *******************************************************************************/
 package ch.res_ear.samthiriot.knime.shapefilesaswkt.writeToDB;
 
 import java.io.File;
