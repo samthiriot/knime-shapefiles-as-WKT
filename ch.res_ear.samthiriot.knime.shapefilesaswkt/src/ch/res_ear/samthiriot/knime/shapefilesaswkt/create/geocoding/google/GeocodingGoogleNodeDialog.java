83884fae0f1a56019f5730422dcac2adcc89cf50
Sam Thiriot <samuel.thiriot@res-ear.ch>
2019-06-17 10:24:37 +0200

    added nodes for geocoding, and added cache for google (fix #57)

---
+++ ch.res_ear.samthiriot.knime.shapefilesaswkt/src/ch/res_ear/samthiriot/knime/shapefilesaswkt/create/geocoding/google/GeocodingGoogleNodeDialog.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2019 EIfER[1] (European Institute for Energy Research).
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU GENERAL PUBLIC LICENSE
+ * which accompanies this distribution, and is available at
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Contributors:
+ *     Samuel Thiriot - original version and contributions
+ *******************************************************************************/
+package ch.res_ear.samthiriot.knime.shapefilesaswkt.create.geocoding.google;
+
+import org.knime.core.data.StringValue;
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
+import org.knime.core.node.defaultnodesettings.DialogComponentString;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+/**
+ * This is an example implementation of the node dialog of the
+ * "SpatialPropertySurface" node.
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
+ * {@link org.knime.core.node.NodeDialogPane}. In general, one can create an
+ * arbitrary complex dialog using Java Swing.
+ * 
+ * @author Samuel Thiriot
+ */
+public class GeocodingGoogleNodeDialog extends DefaultNodeSettingsPane {
+
+	/**
+	 * New dialog pane for configuring the node. The dialog created here
+	 * will show up when double clicking on a node in KNIME Analytics Platform.
+	 */
+    @SuppressWarnings("unchecked")
+	protected GeocodingGoogleNodeDialog() {
+        super();
+        
+        createNewGroup("Address");
+     	addDialogComponent(new DialogComponentColumnNameSelection(
+     			new SettingsModelString(
+     					"colname_address", 
+     					"address"
+     					), 
+     			"column for address", 
+     			0,
+     			true,
+     			StringValue.class
+     			));
+     	
+        createNewGroup("Google API");
+
+     	addDialogComponent(new DialogComponentString(
+     			new SettingsModelString(
+     					"api_key", 
+     					null
+     					),
+        		"API Key",
+        		true,
+        		50
+        		));
+    }
+}
+
---
+++ ch.res_ear.samthiriot.knime.shapefilesaswkt/src/ch/res_ear/samthiriot/knime/shapefilesaswkt/create/geocoding/google/GoogleGeocodingCache.java
@@ -0,0 +1,48 @@
+package ch.res_ear.samthiriot.knime.shapefilesaswkt.create.geocoding.google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.knime.core.node.NodeLogger;
+
+import com.google.maps.model.GeocodingResult;
+
+import ch.res_ear.samthiriot.knime.shapefilesaswkt.create.geocoding.GeocodingCache;
+
+public class GoogleGeocodingCache extends GeocodingCache<List<GeocodingResult>> {
+
+	private static GoogleGeocodingCache singleton = null;
+	
+	public static GoogleGeocodingCache getInstance() {
+		if (singleton == null)
+			singleton = new GoogleGeocodingCache();
+		return singleton;
+	}
+	
+	private final NodeLogger logger = NodeLogger.getLogger(GoogleGeocodingCache.class);
+
+	public static final String TABLE_NAME_GOOLE_GEOCODING = "GOOGLE_GEOCODING";
+
+	private GoogleGeocodingCache() {
+		
+		super("OpenStreetMap", TABLE_NAME_GOOLE_GEOCODING);
+		
+	}
+
+	@Override
+	public void storeInCache(String address, List<GeocodingResult> result) {
+		
+		List<GeocodingResult> copy = new ArrayList<>(result.size());
+		
+		// remove what cannot be serialized: google plus code (useless in our case)
+		for (int i=0; i<result.size(); i++) {
+			GeocodingResult r = result.get(i);
+			r.plusCode = null;
+			copy.add(r);
+		}
+		super.storeInCache(address, copy);
+	}
+	
+	
+
+}
